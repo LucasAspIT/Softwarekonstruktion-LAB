a447374c6406b2907b009a7cc53e224b3609f84f
LucasAspIT <luca609a@edu.campusvejle.dk>
2021-04-14 15:00:05 +0200

    Continued assignment

--- Isbutik 1/Isbutik 1/MainWindow.xaml.cs
+++ Isbutik 1/Isbutik 1/MainWindow.xaml.cs
@@ -21,28 +21,88 @@ namespace Isbutik_1 {
         public MainWindow() {
             InitializeComponent();
 
+            // Sets the default source of the bindings to MainWindow
             DataContext = this;
-            // dgOrderInfo.ItemsSource = OrderList;
+
+            // Run the method with the product information
+            ComboboxIceList();
         }
 
+        public Product ChosenProducts { get; set; }
+
+        // Create a list that holds items from the "Order" class
         public List<Order> OrderList { get; set; } = new List<Order>();
 
+        // Create a list that holds items from the "Product" class
+        public List<Product> ProductOverview { get; set; } = new List<Product>();
+
+        /// <summary>
+        /// Creates the products with their name, description and price.
+        /// </summary>
+        private void ComboboxIceList() {
+            ProductOverview.Add(new Product() { Name = "Magnum", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt sodales augue a laoreet. Nulla consectetur pharetra justo ut finibus. Duis auctor, urna nec consequat imperdiet.", UnitPrice = 11.5 });
+            ProductOverview.Add(new Product() { Name = "Astronaut", Description = "Lorem ipsum curabitur massa ipsum, egestas sed orci eu, consequat pretium neque. Nullam eget tempor massa, sit amet faucibus enim. Integer accumsan metus ac ex sodales, sit amet dapibus.", UnitPrice = 9.50 });
+            ProductOverview.Add(new Product() { Name = "Lillebror", Description = "Lorem ipsum aenean tincidunt mi mauris. Nullam elit nunc, luctus ac velit sit amet, imperdiet ornare nisi. Sed ut nulla nec mi imperdiet malesuada in elit vel pulvinar venenatis efficitur.", UnitPrice = 7.00 });
+            ProductOverview.Add(new Product() { Name = "Kung Fu", Description = "Lorem ipsum orci id laoreet placerat, ligula leo hendrerit nisl, sit amet maximus nulla orci in libero non pellentesque erat, ut venenatis maecenas lobortis posuere dolor ut finibus gravida.", UnitPrice = 8.00 });
+        }
+
         private void cbxIceChoice_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            ChosenProducts = e.AddedItems[0] as Product;
 
+            // Change to the product description
+            tbDescription.Text = ChosenProducts.Description;
+
+            // Create a formatted string of the unit price, properly showing two (N2) decimals
+            string formattedPrice = String.Format("{0:N2}", ChosenProducts.UnitPrice);
+            tbPrice.Text = formattedPrice;
         }
 
-        private void tbxIceAmount_TextChanged(object sender, TextChangedEventArgs e) {
-
-        }
-
+        /// <summary>
+        /// When the button "btnIceAdd" is clicked, add a new "Order" to the "OrderList" that contains the name and the amount of the ice cream, then refresh the list.
+        /// </summary>
         private void btnIceAdd_Click(object sender, RoutedEventArgs e) {
-            // OrderList.Add(cbxIceChoice.SelectionBoxItem.ToString());
-            // Order OrderList = new Order(cbxIceChoice.SelectionBoxItem.ToString(), int.Parse(tbxIceAmount.Text));
+            try {
+                int selectedAmount = int.Parse(tbxIceAmount.Text);
+
+                if (selectedAmount > 0 && selectedAmount <= 255) {
+                    OrderList.Add(new Order() { Name = cbxIceChoice.SelectedValue.ToString(), Amount = selectedAmount });
+                    dgOrderInfo.Items.Refresh();
+                }
+                else if (selectedAmount <= 0) {
+                    MessageBox.Show("Vælg venligst minimum 1 is.");
+                }
+                else if (selectedAmount > 255) {
+                    MessageBox.Show("Vælg venligst højst 255 is ad gangen.");
+                }
+                else {
+                    MessageBox.Show("Ukendt fejl.");
+                }
+            }
+            catch (FormatException) {
+                MessageBox.Show("Indtast venligst et gyldigt nummer.");
+            }
+            catch (OverflowException) {
+                MessageBox.Show("Vælg venligst et antal mellem 1 og 255.");
+            }
+        }
+
+        /// <summary>
+        /// Delete the selected list item using the currently selected index in the datagrid
+        /// </summary>
+        private void btnRemove_Click(object sender, RoutedEventArgs e) {
+            try {
+            OrderList.RemoveAt(dgOrderInfo.SelectedIndex);
             dgOrderInfo.Items.Refresh();
+            }
+            // If nothing is selected, and therefore the index is out of range
+            catch (ArgumentOutOfRangeException) {
+                MessageBox.Show("Vælg det fra listen du vil slette.");
+            }
         }
 
         private void btnOrder_Click(object sender, RoutedEventArgs e) {
 
         }
+
     }
 }
--- Isbutik 1/Isbutik 1/Product.cs
+++ Isbutik 1/Isbutik 1/Product.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 namespace Isbutik_1 {
 
     /// <summary>
-    /// Needs a string Name, and int Amount.
+    /// Needs a string Name, string Description and double UnitPrice.
     /// </summary>
-    public class Order {
+    public class Product {
 
         public string Name { get; set; }
 
-        public int Amount { get; set; }
+        public string Description { get; set; }
+
+        public double UnitPrice { get; set; }
     }
 }
